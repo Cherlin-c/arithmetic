6272e83b71c68d89eaac39c7d8b7bbf233271a18
scl <s2321274@163.com>
2022-08-13 21:49:14 +0800

    init

---
+++ src/main/java/com/scl/Mainn.java
@@ -0,0 +1,13 @@
+package com.scl;
+
+/**
+ * main
+ *
+ * @author : scl
+ * @date :  2022/8/13
+ */
+public class Mainn {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
