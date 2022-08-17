79fe14adfa3a58fe2ea160c16c01b1db15323ee2
scl <s2321274@163.com>
2022-08-17 19:49:19 +0800

    Day2-单链表2 面试题

---
+++ src/main/java/com/scl/hot100/Mid19.java
@@ -0,0 +1,75 @@
+package com.scl.hot100;
+
+/**
+ * 19. 删除链表的倒数第 N 个结点
+ *
+ * @author : scl
+ * @date :  2022/8/16
+ */
+public class Mid19 {
+    /*
+        给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+        输入：head = [1,2,3,4,5], n = 2
+        输出：[1,2,3,5]
+        输入：head = [1], n = 1
+        输出：[]
+        输入：head = [1,2], n = 1
+        输出：[1]
+        提示：
+        链表中结点的数目为 sz
+        1 <= sz <= 30
+        0 <= Node.val <= 100
+        1 <= n <= sz
+     */
+
+    public static void main(String[] args) {
+//        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+        ListNode listNode = new ListNode(1, new ListNode(2));
+        System.out.println("removeNthFromEnd(listNode,2) = " + removeNthFromEnd(listNode, 1));
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode newList = new ListNode(0, head);
+        int i = 1;
+        ListNode temp = head;
+        while (temp.next != null) {
+            i++;
+            temp = temp.next;
+        }
+        ListNode temp2 = newList;
+        for (int j = 1; j <= i; j++) {
+            if (i - j == n - 1) {
+                temp2.next = temp2.next.next;
+                break;
+            }
+            temp2 = temp2.next;
+        }
+        head = newList.next;
+        return head;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
--- src/main/java/com/scl/linkedList/single/Interview.java
+++ src/main/java/com/scl/linkedList/single/Interview.java
@@ -1,5 +1,7 @@
 package com.scl.linkedList.single;
 
+import java.util.Stack;
+
 /**
  * 面试题
  *
@@ -27,25 +29,14 @@ public class Interview {
         //        求单链表的节点个数(不含头节点)
         //    ===========    1 BEGIN    ==============
         System.out.println("==============1================");
-        System.out.println("linkedList.getLength() = " + linkedList.getLength());
+        System.out.println("单链表的节点个数(不含头节点) = " + method1(linkedList));
         //    ===========    1 END    ==============
 
         //        查找单链表中倒数第K个节点
         //    ===========    2 BEGIN    ==============
         System.out.println("==============2================");
-        int k = 1;
-        int index = 0;
-        int length = linkedList.getLength();
-        Node node = linkedList.head.next;
-        while (node != null) {
-            if (k == (length - index)) {
-                System.out.println("node = " + node);
-                break;
-            } else {
-                node = node.next;
-                index++;
-            }
-        }
+        int k = 2;
+        System.out.println("单链表中倒数第" + k + "个节点 = " + method2(linkedList, k));
         //    ===========    2 END    ==============
 
         //        单链表的反转
@@ -68,5 +59,46 @@ public class Interview {
         linkedList = reverseLinkedList;
         linkedList.list();
         //    ===========    3 END    ==============
+
+        //        从尾到头打印单链表：要求1反向遍历，2Stack栈
+        //    ===========    4 BEGIN    ==============
+        System.out.println("==============4================");
+
+        Stack<Node> nodes = new Stack<Node>();
+        Node node = linkedList.head;
+        while (node.next != null) {
+//            nodes.add(node.next); 意思相同 来自不同类
+            nodes.push(node.next);
+            node = node.next;
+        }
+        for (int size = nodes.size(); size > 0; size--) {
+            System.out.println(nodes.pop());
+        }
+        //    ===========    4 END    ==============
+
+        //        合并两个有序的单链表，合并之后依然有序
+        //    ===========    5 BEGIN    ==============
+        System.out.println("==============5================");
+
+        //    ===========    5 END    ==============
+    }
+
+    private static int method1(SingleLinkedList linkedList) {
+        return linkedList.getLength();
+    }
+
+    private static Node method2(SingleLinkedList linkedList, int k) {
+        int index = 0;
+        int length = linkedList.getLength();
+        Node node = linkedList.head.next;
+        while (node != null) {
+            if (k == (length - index)) {
+                return node;
+            } else {
+                node = node.next;
+                index++;
+            }
+        }
+        return node;
     }
 }
